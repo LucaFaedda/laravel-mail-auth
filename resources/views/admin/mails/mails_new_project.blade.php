21d71cfe52f4690e56883bc133cc8a4386d96f0f
LucaFaedda <lucakarl91@gmail.com>
2023-03-08 17:11:40 +0100

    modificato stile mail

--- resources/views/admin/mails/mails_new_project.blade.php
+++ resources/views/admin/mails/mails_new_project.blade.php
@@ -1,7 +1,38 @@
-<div>
-    <h1>Nuovo progetto</h1>
-    <p>Titolo: {{$lead->title}}</p>
-    <p>Data del progetto: {{$lead->data_progetto}}</p>
-    <p>Difficoltà: {{$lead->difficoltà}}</p>
-    <p>Descrizione: {{$lead->descrizione}}</p>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body >
+    <div >
+        <h1>Nuovo progetto</h1>
+        <h4><span>Titolo</span> {{$lead->title}}</h4>
+        <hr>
+        <p class="bold"><span>Data del Progetto</span> {{$lead->data_progetto}}</p>
+        <p class="bold"><span>Descrizione: </span> {{$lead->descrizione}}</p>
+        <p class="bold"><span>Difficoltà:</span> {{$lead->difficoltà}}</p>
+    </div>
+    
+</body>
+</html>
+
+<style>
+    body{
+        background-color: #F3F7F0;
+    }
+    h1{
+        color: rgb(100, 32, 32);
+        font-family: 'Courier New', Courier, monospace;
+    }
+
+    hr{
+        color: #BBE6E4;
+    }
+
+    .bold{
+        font-weight: 700;
+    }
+</style>
